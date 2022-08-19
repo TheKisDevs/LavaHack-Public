7a203a293ab389644e0e25a0d89c3e216ceb35c8
It's kisman <81470009+kisman2000@users.noreply.github.com>
2022-08-19 19:07:55 +0300

    Cleanupped JesusPlus

--- src/main/java/com/kisman/cc/module/movement/JesusPlus.java
+++ src/main/java/com/kisman/cc/module/movement/JesusPlus.java
@@ -11,27 +11,25 @@ import net.minecraft.util.math.BlockPos;
 
 import java.util.Arrays;
 
-
 public class JesusPlus extends Module {
-
      public int waterTicks = 0;
-    public final Setting speedX = register(new Setting("Jesus Speed:", this, 1.0f, 0.01f, 5.0f,  false));
-    private final Setting jesusmode = register(new Setting("Jesus Mode", this, "Matrix Zoom", Arrays.asList("Matrix Zoom", "SunRise", "Packet")));
-    public JesusPlus(){
+     public final Setting speedX = register(new Setting("Speed", this, 1.0f, 0.01f, 5.0f,  false));
+     private final Setting jesusmode = register(new Setting("Mode", this, "Matrix Zoom", Arrays.asList("Matrix Zoom", "SunRise", "Packet")));
+     
+     public JesusPlus(){
         super("JesusPlus", Category.MOVEMENT);
-    }
-    public void update(){
-        if(mc.player == null || mc.world == null) return;
-        if (Jesus.mc.world.getBlockState(new BlockPos(Jesus.mc.player.posX, Jesus.mc.player.posY - (double)0.2f, Jesus.mc.player.posZ)).getBlock() instanceof BlockLiquid && !Jesus.mc.player.onGround) {
-            float Speedz = speedX.getValFloat();
-            PlayerUtil.setSpeed(mc.player, Speedz);
-
-        }
-        if (!(Jesus.mc.world.getBlockState(new BlockPos(Jesus.mc.player.posX, Jesus.mc.player.posY + (double)1.0E-4f, Jesus.mc.player.posZ)).getBlock() instanceof BlockLiquid)) return;
-        Jesus.mc.player.motionX = 0.0;
-        Jesus.mc.player.motionZ = 0.0;
-        Jesus.mc.player.motionY = 0.05f;
-
-
-    }
-    }
+     }
+     
+     public void update() {
+          if(mc.player == null || mc.world == null) return;
+          
+          if (mc.world.getBlockState(new BlockPos(mc.player.posX, mc.player.posY - (double)0.2f, mc.player.posZ)).getBlock() instanceof BlockLiquid && !mc.player.onGround) {
+               float Speedz = speedX.getValFloat();
+               PlayerUtil.setSpeed(mc.player, Speedz);
+          }
+          if (!(mc.world.getBlockState(new BlockPos(mc.player.posX, mc.player.posY + (double)1.0E-4f, mc.player.posZ)).getBlock() instanceof BlockLiquid)) return;
+          mc.player.motionX = 0.0;
+          mc.player.motionZ = 0.0;
+          mc.player.motionY = 0.05f;
+     }
+}
