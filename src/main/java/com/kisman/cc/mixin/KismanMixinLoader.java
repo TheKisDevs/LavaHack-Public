9311e3d78105135bf3fac10e1d81f9d2651203cd
It's kisman <81470009+kisman2000@users.noreply.github.com>
2023-02-25 21:36:42 +0300

    fixed mixins

--- src/main/java/com/kisman/cc/mixin/KismanMixinLoader.java
+++ src/main/java/com/kisman/cc/mixin/KismanMixinLoader.java
@@ -38,9 +38,7 @@ public class KismanMixinLoader implements IFMLLoadingPlugin {
 
     @Override
     public void injectData(Map<String, Object> data){
-        MixinBootstrap.init();
-        Mixins.addConfiguration("mixins.kisman.json");
-        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+        
     }
 
     @Override
